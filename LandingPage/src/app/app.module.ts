b10863d1caa8b06abaedc2879185c84d08d9349c
Devin-Jackson314 <you@example.com>
2022-08-27 13:47:04 -0400

    still building components

--- LandingPage/src/app/app.module.ts
+++ LandingPage/src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { DesignSolutionComponent } from './design-solution/design-solution.component';
+import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
+import { AppsComponent } from './apps/apps.component';
+import { IllustrationsComponent } from './illustrations/illustrations.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { MotionGraphicsComponent } from './motion-graphics/motion-graphics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
-    DesignSolutionComponent
+    DesignSolutionComponent,
+    GraphicDesignComponent,
+    AppsComponent,
+    IllustrationsComponent,
+    PhotographyComponent,
+    MotionGraphicsComponent
   ],
   imports: [
     BrowserModule
